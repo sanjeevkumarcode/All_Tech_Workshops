11015ff229faa1f4a8188080fdf8bf3ffba39572
Sanjeev Kumar <70787360+sanjeevkumarcode@users.noreply.github.com>
2022-09-08 01:32:48 +0530

    Delete Python/12. Project Sample directory

--- Python/12. Project Sample/test2.py
+++
@@ -1,4 +0,0 @@
-import  logging
-
-logging.basicConfig(filename="test2.log",level=logging.DEBUG ,format='%(levelname)s %(asctime)s %(name)s  %(message)s')
-logging.info("this is my log with timestamp")
